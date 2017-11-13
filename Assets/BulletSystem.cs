c4c89b9368807f6140675083f10251bdddfd122e
Unknown <grooney4@gmail.com>
2017-11-12 19:57:35 -0500

    Created Particle System for lasers and log where hitting world

---
+++ Assets/BulletSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSystem : MonoBehaviour {
+    
+    
+
+    void OnParticleCollision(GameObject other)
+    {
+        List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>(); 
+        GetComponent<ParticleSystem>().GetCollisionEvents(other, collisionEvents);
+        print(collisionEvents[0].intersection);
+        
+    }
+}
