f258146238aee3b4c27c666d52f1c2bbf9bc8e97
Unknown <grooney4@gmail.com>
2017-11-19 20:13:23 -0500

    Modified MusicPlayer.cs to load or unload music players,
    added explosion to player.cs

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CollisionHandler : MonoBehaviour
+{
+    [Tooltip("in Seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("Particle Effect on Player")][SerializeField] GameObject deathFX;
+    void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+       
+        SendMessage("OnPlayerDeath");
+        deathFX.SetActive(true);
+        Invoke("ReLoadScene", levelLoadDelay);
+
+    }
+    private void ReLoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class PlayerController : MonoBehaviour {
+    [Header("General")]
+    [Tooltip("In metres per second")][SerializeField] float Speed = 4f;
+    [Tooltip("in metres")] [SerializeField] float xRange = 5f;
+    [Tooltip("in metres")] [SerializeField] float yRange = 3f;
+    [Header("Screen Position")]
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = 5f;
+    [Header("Control Throw")]
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float controlRollFactor = -20f;
+    float xThrow, yThrow;
+    bool isControlsEnabled = true;
+    
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (isControlsEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+    }
+    void OnPlayerDeath() //called by string reference in CollisionHandler
+    {
+        isControlsEnabled = false;
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch =  pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * Speed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * Speed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
