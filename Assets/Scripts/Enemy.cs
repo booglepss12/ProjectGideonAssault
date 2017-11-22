367c77f9e308b1b4c2c6a89a46c91aaaa44a511c
Unknown <grooney4@gmail.com>
2017-11-21 20:05:01 -0500

    Created enemy death FX

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField] GameObject enemyDeathFx;
+    [SerializeField] Transform parent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Collider enemyBoxCollider = gameObject.AddComponent<BoxCollider>();
+        enemyBoxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(enemyDeathFx, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
